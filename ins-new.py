378ac2db1365ff5e70d88642c5ad2b6e16d1cbb2
premaramach <118570124+premaramach@users.noreply.github.com>
2023-02-23 15:53:22 -0500

    Update ins-new.py

--- ins-new.py
+++ ins-new.py
@@ -3,11 +3,11 @@ import sys
 
 region = sys.argv[1]
 accesskey = sys.argv[2]
-#secretkey = sys.argv[3]
+secretkey = sys.argv[3]
 
 
-#client = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
-client = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key="UOdm+CfSOj4Ydmso0FxklmzuBv0AfMgSB+jXmQBw")
+client = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
+#client = boto3.client('ec2',region_name=region,aws_access_key_id=accesskey,aws_secret_access_key="UOdm+CfSOj4Ydmso0FxklmzuBv0AfMgSB+jXmQBw")
 
 data1 = client.describe_instances()
 for data2 in data1["Reservations"]:
